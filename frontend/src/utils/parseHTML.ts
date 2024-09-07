3ff962ee1a69360dc2d36607d1fe5b62c0a6af8a
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-07 18:06:30 +0900

    Update parseHTML.ts

--- frontend/src/utils/parseHTML.ts
+++ frontend/src/utils/parseHTML.ts
@@ -1,20 +1,37 @@
+/**
+ * HTML文字列を解析し、特定のクラス名を持つ要素のテキストを抽出する関数
+ * @param {string} html - 解析対象のHTML文字列
+ * @returns {Object} - 抽出されたデータを含むオブジェクト
+ */
 export default function parseHTML(html: string) {
   const parser = new DOMParser();
   const document = parser.parseFromString(html, "text/html");
 
-  const time: any[] = [];
-  document.querySelectorAll(".time").forEach((element) => {
-    time.push(element.textContent);
+  // 出発時間や到着時間を取得し、そのテキストを配列に格納
+  const time: string[] = [];
+  document.querySelectorAll(".time").forEach((el) => {
+    time.push(el.textContent || "");
   });
 
-  const train: any[] = [];
-  document.querySelectorAll(".transport").forEach((icon) => {
-    train.push(icon.textContent);
+  // 駅名を取得し、そのテキストを配列に格納
+  const station: string[] = [];
+  document
+    .querySelectorAll(".station dt")
+    .forEach((el) => station.push(el.textContent || ""));
+
+  // 鉄道や方面を取得し、そのテキストを配列に格納
+  const transport: object[] = [];
+  document.querySelectorAll(".transport").forEach((el) => {
+    transport.push({
+      line: el.querySelector("div")?.textContent,
+      destination: el.querySelector(".destination")?.textContent,
+    });
   });
 
-  const summary = Array.from(
+  // 距離や運賃などの概要を取得し、それぞれのテキストを配列に格納
+  const summary: string[] = Array.from(
     document.querySelector(".summary")?.children || []
-  ).map((element) => element.textContent);
+  ).map((el) => el.textContent || "");
 
-  return { time, train, summary };
+  return { station, time, transport, summary };
 }
