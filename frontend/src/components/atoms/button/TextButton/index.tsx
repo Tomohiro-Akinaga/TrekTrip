16e6b28a831868f4a71458e1ffe2816f095c6465
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-07 18:16:35 +0900

    Update index.tsx

--- frontend/src/components/atoms/button/TextButton/index.tsx
+++ frontend/src/components/atoms/button/TextButton/index.tsx
@@ -9,13 +9,21 @@ const TextButton = ({ children }: PropsWithChildren<Props>) => {
   const handlClick = async () => {
     const html = await fetch("/api/scrape", {
       method: "POST",
-      body: JSON.stringify({ departure: "東京駅", arrival: "大阪駅" }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        departure: "東京駅",
+        arrival: "大阪駅",
+        year: "2024",
+        month: "12",
+        day: "31",
+        hour: "12",
+        minutes: "10",
+        seconds: "03",
+      }),
     });
 
     const json = await html.json();
-    console.log(json);
-
-    const text = parseHTML(json.html as any);
+    const text = parseHTML(json.html);
     console.log(text);
   };
   return <div onClick={handlClick}>{children}</div>;
