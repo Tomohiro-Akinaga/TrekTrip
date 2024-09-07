b68ccf92f9f5aba831b9dead6da989270d040434
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-07 13:36:48 +0900

    Update route.ts

--- frontend/src/app/api/scrape/route.ts
+++ frontend/src/app/api/scrape/route.ts
@@ -10,10 +10,22 @@ export async function POST(request: NextRequest) {
   await page.goto("https://transit.yahoo.co.jp/");
 
   /* 出発駅入力 */
-  await page.locator('#query_input[name="from"]').fill("出発駅");
+  await page.locator('#query_input[name="from"]').fill("東京");
 
   /* 到着駅入力 */
-  await page.locator('#query_input[name="to"]').fill("到着駅");
+  await page.locator('#query_input[name="to"]').fill("福島駅");
+
+  /* 全交通手段のチェックを外す */
+  const transitCheckboxes = await page.$$('input[type="checkbox"]');
+  for (const checkbox of transitCheckboxes) {
+    const isChecked = await checkbox.evaluate((element) => element.checked);
+    if (!isChecked) return;
+    await checkbox.click();
+  }
+
+  /* 検索クリック */
+  await page.locator("#searchModuleSubmit").click();
+  await page.waitForNavigation();
 
   /* ブラウザ終了 */
   await browser.close();
