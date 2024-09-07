5cb75d862344000058b9098fbf70f2231779bbb3
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-07 14:15:36 +0900

    Update route.ts

--- frontend/src/app/api/scrape/route.ts
+++ frontend/src/app/api/scrape/route.ts
@@ -2,33 +2,42 @@ import puppeteer from "puppeteer";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  /* ブラウザ起動 */
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  /* リクエストボディから出発駅と到着駅を取得 */
+  const { departure, arrival } = await request.json();
+
+  /* Puppeteerでブラウザを起動する */
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(3000);
 
-  /* Yahoo乗り換え案内へ移動 */
-  await page.goto("https://transit.yahoo.co.jp/");
+  try {
+    /* Yahooの乗り換え案内ページにアクセス */
+    await page.goto("https://transit.yahoo.co.jp/");
 
-  /* 出発駅入力 */
-  await page.locator('#query_input[name="from"]').fill("東京");
+    /* 出発駅と到着駅の入力フィールドにリクエスト値を入力 */
+    await page.locator('#query_input[name="from"]').fill(departure);
+    await page.locator('#query_input[name="to"]').fill(arrival);
 
-  /* 到着駅入力 */
-  await page.locator('#query_input[name="to"]').fill("福島駅");
+    /* 交通手段（新幹線、バスなど）の選択をリセット（チェックされている項目をすべて外す） */
+    const transitCheckboxes = await page.$$('input[type="checkbox"]');
+    for (const checkbox of transitCheckboxes) {
+      const isChecked = await checkbox.evaluate((element) => element.checked);
+      if (isChecked) await checkbox.click();
+    }
 
-  /* 全交通手段のチェックを外す */
-  const transitCheckboxes = await page.$$('input[type="checkbox"]');
-  for (const checkbox of transitCheckboxes) {
-    const isChecked = await checkbox.evaluate((element) => element.checked);
-    if (!isChecked) return;
-    await checkbox.click();
+    /* 検索ボタンをクリック */
+    await page.locator("#searchModuleSubmit").click();
+    await page.waitForNavigation();
+    await page.waitForSelector("#route01");
+
+    /* ページのHTMLを取得*/
+    const html = await page.content();
+
+    return NextResponse.json({ html }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  } finally {
+    /* ブラウザを終了 */
+    await browser.close();
   }
-
-  /* 検索クリック */
-  await page.locator("#searchModuleSubmit").click();
-  await page.waitForNavigation();
-
-  /* ブラウザ終了 */
-  await browser.close();
-
-  return NextResponse.json({ message: "Scraped" }, { status: 201 });
 }
