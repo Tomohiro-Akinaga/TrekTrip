f3a4c08616a2f121e8f2b0b4c81a428f65efc537
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-09-10 12:34:38 +0900

    Update index.tsx

--- frontend/src/components/organisms/Header/index.tsx
+++ frontend/src/components/organisms/Header/index.tsx
@@ -1,12 +1,68 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
+"use client";
+
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <Button
+          style={{ color: "rgba(0, 0, 0, 0.54)", background: "transparent" }}
+        >
+          <CloseIcon></CloseIcon>
+        </Button>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -16,9 +72,13 @@ const Header = () => {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+          onClick={toggleDrawer(true)}
         >
           <MenuIcon />
         </IconButton>
+        <Drawer open={open} onClose={toggleDrawer(false)} variant="temporary">
+          {DrawerList}
+        </Drawer>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           青春18きっぷ乗り換え案内
         </Typography>
